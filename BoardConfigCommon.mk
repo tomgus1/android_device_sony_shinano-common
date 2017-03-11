5d846e6cc57a34ac395c94f7c86f5eeb07ddaab5
nailyk-fr <nailyk_git@nailyk.fr>
2017-03-11 17:22:54 +0100

    shinano-common: nfc: Update for Lineage 14.1
    Change-Id: I2dcc20172815a3e79940c5e5b40daa8faf2b6031

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -80,6 +80,8 @@ WIFI_DRIVER_FW_PATH_STA     := "/vendor/firmware/fw_bcmdhd.bin"
 # NFC
 BOARD_NFC_CHIPSET := pn547
 BOARD_NFC_DEVICE := /dev/pn547
+PN547C2 := 1
+NFC_NXP_CHIP_TYPE := PN547C2
 
 # Filesystem
 BOARD_FLASH_BLOCK_SIZE := 131072
